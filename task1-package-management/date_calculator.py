3a079ca5106eb5e8b609ab840a27b5b6d47ab540
theodomir <ukundasetheodomir@gmail.com>
2025-05-02 14:43:38 +0000

    Date calculator applying module date time

--- task1-package-management/date_calculator.py
+++ task1-package-management/date_calculator.py
@@ -1,12 +1,15 @@
-from datetime import timedelta
-delta = timedelta(
-    days=50,
-    seconds=27,
-    microseconds=10,
-    milliseconds=29000,
-    minutes=5,
-    hours=8,
-    weeks=2
-)
-# Only days, seconds, and microseconds remain
-delta
+from datetime import datetime
+
+def date_difference(date1: str, date2: str) -> int:
+    try:
+        d1 = datetime.strptime(date1, "%Y-%m-%d")
+        d2 = datetime.strptime(date2, "%Y-%m-%d")
+        return (d2 - d1).days
+    except ValueError as e:
+        raise ValueError(f"Invalid date format. Please use 'YYYY-MM-DD'. Error: {e}")
+try:
+    d1=input(print("please Enter first date in YYYY-MM-DD format"))
+    d2=input(print("please Enter second date in YYYY-MM-DD format"))
+    print(date_difference(d1,d2))  
+except ValueError as err:
+    print("Error")
